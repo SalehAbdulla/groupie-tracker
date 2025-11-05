352e99a37c0648870034374d290aba8a21a79d99
Saleh Abdulla <salehabdullaict@gmail.com>
2025-11-05 19:47:11 +0300

    Card Data Handler logic added

--- internal/handlers/CardData.go
+++ internal/handlers/CardData.go
@@ -1,20 +1,22 @@
 package handlers
 
 import (
-	"fmt"
 	"groupie-tracker/internal/constants"
+	"strconv"
 
 	"net/http"
 )
 
 func (h *Handlers) CardData(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != fmt.Sprintf("/CardPageData/{%d}", id) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if !(id >= 1 && id <= len(h.Artists)) || err != nil {
 		h.NotFound(w, r)
 		return
 	}
 	if r.Method != http.MethodGet {
-		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		h.render(w, "ErrorPage.html", constants.Error{Error: http.StatusText(http.StatusMethodNotAllowed)})
 		return
 	}
-	h.render(w, "CardPageData.html", constants.CardPageData{})
+	h.render(w, "CardPageData.html", constants.HomePageData{Artists: h.Artists})
 }
